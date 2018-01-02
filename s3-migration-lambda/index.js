ad848ac8be08d2cf9337631ca10dbde82956ed56
Abhaya Chauhan <abschauhan@gmail.com>
2018-01-02 23:05:22 +1100

    Fixed lambda bugs

--- s3-migration-lambda/index.js
+++ s3-migration-lambda/index.js
@@ -1,14 +1,39 @@
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB();
 var newTableName = process.env.destination;
+var keySchema = [];
 
 exports.handler = (event, context, callback) => {
-    var promises = [];
+  if (keySchema.length) {
+    pushItems(event, context, callback);
+  }
+  else 
+  {
+    dynamodb.describeTable({ TableName: newTableName }, (err, data) => {
+      if (err)
+        return callback(err);
+        
+      console.log('Describing table');
+      data.Table.KeySchema.forEach((key) => {
+        keySchema.push(key.AttributeName);
+      });
+      pushItems(event, context, callback);
+    });
+  }
+}
 
+function pushItems (event, context, callback) {
+    var promises = [];
     event.Records.forEach(function(record) {
+
       if (record.eventName === "REMOVE") {
+        
+          var keyParams = {};
+          keySchema.forEach((key) => {
+            keyParams[key] = record.dynamodb.OldImage[key];
+          });
           var params = {
-            Key: record.dynamodb.Keys,
+            Key: keyParams,
             TableName: newTableName
           };
 
